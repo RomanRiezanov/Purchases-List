dd862f1fbe0f4eeeebf1b56d848cc6c0918f2714
Roman Riezanov <romark6@gmail.com>
2022-07-26 14:19:13 +0300

    Added sorting by number and by price

--- src/components/main/purhcases-list/purchases-list-header/PurchasesListHeader.tsx
+++ src/components/main/purhcases-list/purchases-list-header/PurchasesListHeader.tsx
@@ -1,11 +1,34 @@
+import { sortByFunc } from "../PurchasesList";
 import styles from "./PurchasesListHeader.module.scss";
 
-const PurchasesListHeader = () => {
+type PurchasesListHeaderProps = {
+  sortBy: sortByFunc;
+};
+
+const PurchasesListHeader = ({ sortBy }: PurchasesListHeaderProps) => {
   return (
     <ul className={styles.list}>
-      <li className={styles.list__item}>№</li>
+      <li className={styles.list__item}>
+        №
+        <div onClick={() => sortBy("number")}>
+          <img
+            className={styles.img}
+            src="./assets/icons/sort-down.svg"
+            alt="sort-down"
+          />
+        </div>
+      </li>
       <li className={styles.list__item}>Name</li>
-      <li className={styles.list__item}>Spent</li>
+      <li className={styles.list__item}>
+        Spent
+        <div onClick={() => sortBy("price")}>
+          <img
+            className={styles.img}
+            src="./assets/icons/sort-down.svg"
+            alt="sort-down"
+          />
+        </div>
+      </li>
       <li className={styles.list__item}>Date</li>
     </ul>
   );
